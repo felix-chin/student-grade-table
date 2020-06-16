a8a766ace01caad29535acef828b66db8344272b
Felix Chin <felixmchin@gmail.com>
2020-06-16 16:55:25 -0700

    instantiated GradeForm class

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var form = document.querySelector('form');
+var gradeForm = new GradeForm(form);
 var header = document.querySelector('header');
 var pageHeader = new PageHeader(header);
 var table = document.querySelector('table');
