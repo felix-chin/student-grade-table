670b298e20997a55326edeb63f38ae053069be07
Felix Chin <felixmchin@gmail.com>
2020-06-16 14:56:24 -0700

    modified updateGrades method and created loop

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,19 @@ class GradeTable {
     this.tableElement = tableElement;
   }
   updateGrades(grades) {
+    var tbody = this.tableElement.querySelector('tbody');
+    tbody.textContent = '';
+    for(var i = 0; i < grades.length; i++){
+      var row = document.createElement('tr');
+      var student = document.createElement('td');
+      var course = document.createElement('td');
+      var grade = document.createElement('td');
+      student.textContent = grades[i].name;
+      course.textContent = grades[i].course;
+      grade.textContent = grades[i].grade;
+      row.append(student, course, grade);
+      tbody.appendChild(row);
+    }
     console.log(grades);
   }
 }
