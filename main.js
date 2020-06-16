5e8d511ba706cf7d0469424e39fb88d6a1a86481
Felix Chin <felixmchin@gmail.com>
2020-06-16 15:37:39 -0700

    queried header element and instantiated PageHeader class

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var header = document.querySelector('header');
+var pageHeader = new PageHeader(header);
 var table = document.querySelector('table');
 var gradeTable = new GradeTable(table);
 var app = new App(gradeTable);
