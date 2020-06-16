69d6e7e2d8c0cd85cfcbf3fcea9d498f6380db01
Felix Chin <felixmchin@gmail.com>
2020-06-16 16:53:35 -0700

    bind handleSubmit and added eventlistener

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,6 +1,8 @@
 class GradeForm {
   constructor(formElement) {
     this.formElement = formElement;
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.formElement.addEventListener('submit', this.handleSubmit);
   }
   onSubmit(createGrade) {
     this.createGrade = createGrade;
