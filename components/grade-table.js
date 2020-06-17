3147ffcacb02658a988753cbe506a8c30c36988f
Felix Chin <felixmchin@gmail.com>
2020-06-16 17:43:56 -0700

    created renderGradeRow method on GradeTable class

--- components/grade-table.js
+++ components/grade-table.js
@@ -21,4 +21,23 @@ class GradeTable {
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
   }
+  renderGradeRow(data, deleteGrade) {
+    var tbody = this.tableElement.querySelector('tbody');
+    var row = document.createElement('tr');
+    var student = document.createElement('td');
+    var course = document.createElement('td');
+    var grade = document.createElement('td');
+    var delButton = document.createElement('td');
+    var button = document.createElement('button');
+    student.textContent = data.name;
+    course.textContent = data.course;
+    grade.textContent = data.grade;
+    button.textContent = 'DELETE';
+    button.className = 'btn btn-danger'
+    delButton.appendChild(button);
+    row.append(student, course, grade, delButton);
+    tbody.appendChild(row);
+    button.addEventListener('click', deleteGrade(data.id));
+    return row;
+  }
 }
