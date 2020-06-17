02cd8eac5ab3cf501687d94d87d794b39e20ac6e
Felix Chin <felixmchin@gmail.com>
2020-06-16 17:10:03 -0700

    modified App class start method

--- components/app.js
+++ components/app.js
@@ -34,6 +34,7 @@ class App {
   }
   start() {
     this.getGrades();
+    this.gradeForm.onSubmit(this.createGrade);
   }
   createGrade(name, course, grade) {
     console.log(name, course, grade);
