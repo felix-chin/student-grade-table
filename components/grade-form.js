d552dae477c5b410011eb937fe5b534f10799943
Felix Chin <felixmchin@gmail.com>
2020-06-16 17:03:26 -0700

    modified handleSubmit method

--- components/grade-form.js
+++ components/grade-form.js
@@ -9,6 +9,12 @@ class GradeForm {
   }
   handleSubmit(event) {
     event.preventDefault();
+    var formData = new FormData(event.target);
+    var name = formData.get('name');
+    var course = formData.get('course');
+    var grade = formData.get('grade');
+    this.createGrade(name, course, grade)
+    event.target.reset();
     console.log("test message");
   }
 }
