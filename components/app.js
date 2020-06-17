3853e6388933707bd6073e27ff1356d711afb708
Felix Chin <felixmchin@gmail.com>
2020-06-17 15:54:24 -0700

    added feature to update an existing grade

--- components/app.js
+++ components/app.js
@@ -8,6 +8,10 @@ class App {
     this.deleteGrade = this.deleteGrade.bind(this);
     this.handleDeleteGradeError = this.handleDeleteGradeError.bind(this);
     this.handleDeleteGradeSuccess = this.handleDeleteGradeSuccess.bind(this);
+    this.patchGrade = this.patchGrade.bind(this);
+    this.handlePatchGradeError = this.handlePatchGradeError.bind(this);
+    this.handlePatchGradeSuccess = this.handlePatchGradeSuccess.bind(this);
+    this.editGrade = this.editGrade.bind(this);
     this.gradeTable = gradeTable;
     this.pageHeader = pageHeader;
     this.gradeForm = gradeForm;
@@ -43,7 +47,9 @@ class App {
   start() {
     this.getGrades();
     this.gradeForm.onSubmit(this.createGrade);
+    this.gradeForm.onPatch(this.patchGrade);
     this.gradeTable.onDeleteClick(this.deleteGrade);
+    this.gradeTable.onEditClick(this.editGrade);
   }
   createGrade(name, course, grade) {
     $.ajax({
@@ -85,5 +91,43 @@ class App {
   }
   handleDeleteGradeSuccess() {
     this.getGrades();
+  }
+  patchGrade(id, name, course, grade) {
+    $.ajax({
+      type: "PATCH",
+      url: "https://sgt.lfzprototypes.com/api/grades/" + id,
+      data: {
+        "name": name,
+        "course": course,
+        "grade": grade
+      },
+      headers: {
+        "X-Access-Token": "cJdQkJZi"
+      },
+      error: this.handlePatchGradeError,
+      success: this.handlePatchGradeSuccess
+    })
+    console.log(id);
+  }
+  handlePatchGradeError(error) {
+    console.error(error);
+  }
+  handlePatchGradeSuccess() {
+    this.getGrades();
+  }
+  editGrade(id, name, course, grade) {
+    var idInput = document.getElementById('id');
+    var nameInput = document.getElementById('name');
+    var courseInput = document.getElementById('course');
+    var gradeInput = document.getElementById('grade');
+    var addButton = document.getElementById('add');
+    var editButton = document.getElementById('edit');
+    var addHeader = document.querySelector('h4');
+    idInput.value = id;
+    nameInput.value = name;
+    courseInput.value = course;
+    gradeInput.value = grade;
+    addHeader.textContent = 'Update Grade';
+    addButton.textContent = 'Edit';
   }
 }
--- components/grade-form.js
+++ components/grade-form.js
@@ -7,14 +7,28 @@ class GradeForm {
   onSubmit(createGrade) {
     this.createGrade = createGrade;
   }
+  onPatch(patchGrade) {
+    this.patchGrade = patchGrade;
+  }
+
   handleSubmit(event) {
     event.preventDefault();
+    var addButton = document.getElementById('add');
+    var addHeader = document.querySelector('h4');
     var formData = new FormData(event.target);
+    var id = formData.get('id');
     var name = formData.get('name');
     var course = formData.get('course');
     var grade = formData.get('grade');
-    this.createGrade(name, course, grade)
+    if (addButton.textContent === 'Add') {
+      this.createGrade(name, course, grade);
+      console.log("testing add");
+    } else {
+      this.patchGrade(id, name, course, grade);
+      addButton.textContent = 'Add';
+      addHeader.textContent = 'Add Grade';
+      console.log("testing edit");
+    }
     event.target.reset();
-    console.log("test message");
   }
 }
