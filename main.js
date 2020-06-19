a52c6aae0860a287d060015029b9324afdfc077e
Felix Chin <felixmchin@gmail.com>
2020-06-19 12:37:12 -0700

    converted to es6

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-var formElement = document.querySelector('form');
-var gradeForm = new GradeForm(formElement);
-var headerElement = document.querySelector('header');
-var pageHeader = new PageHeader(headerElement);
-var tableElement = document.querySelector('table');
-var noGradesElement = document.querySelector('p');
-var gradeTable = new GradeTable(tableElement, noGradesElement);
-var gradeId = null;
-var gradesArray = [];
-var gradeObj = {};
-var app = new App(gradeTable, pageHeader, gradeForm, gradeId, gradesArray, gradeObj);
+const formElement = document.querySelector('form');
+const gradeForm = new GradeForm(formElement);
+const headerElement = document.querySelector('header');
+const pageHeader = new PageHeader(headerElement);
+const tableElement = document.querySelector('table');
+const noGradesElement = document.querySelector('p');
+const gradeTable = new GradeTable(tableElement, noGradesElement);
+const gradeId = null;
+const gradesArray = [];
+const gradeObj = {};
+const app = new App(gradeTable, pageHeader, gradeForm, gradeId, gradesArray, gradeObj);
 app.start()
